b3bf5b9e455f00e0ebb4c582dc9adb097ad7511c
Thayná de Castro Medeiros <182220014@senacinformatica.br>
2023-11-29 20:57:55 -0300

    Adicionado os models do Cliente e Produto

--- src/Model/ClienteModel.java
+++ src/Model/ClienteModel.java
@@ -1,5 +1,82 @@
 package Model;
 
+import Dao.ConexaoDao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ClienteModel {
+    private int id; 
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }     
+    
+    public static ClienteModel obterClientePorId (int id){
+        String query = "SELECT * FROM CLIENTE WHERE id = ?";
+        ResultSet resultSet = null;
+        ClienteModel cli = null;
+
+        try (Connection conexao = ConexaoDao.conectar();
+            PreparedStatement preparedStatement = conexao.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+         // VERIF SE HÁ RESULTADOS
+            if (resultSet != null && resultSet.next()) {
+             //PREENCHE OBJETO CLIENTE COM OS DADOS DO BANCO DE DADOS
+                cli = new ClienteModel();
+                cli.setId(resultSet.getInt("id"));
+                cli.setCpf(resultSet.getString("cpf"));
+                cli.setNome(resultSet.getString("nome"));
+                cli.setEndereco(resultSet.getString("endereco"));
+            
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace(); 
+        } finally {
+         // FECHA O RESULTSET SE PRECISAR
+        try {
+            if (resultSet != null) resultSet.close();
+        } 
+        catch (SQLException e) {
+            e.printStackTrace(); // EXCESSAO SQL SE FECHAR
+        }
+     }
+     return cli;
+    }
 }
--- src/Model/ProdutoModel.java
+++ src/Model/ProdutoModel.java
@@ -1,5 +1,82 @@
 package Model;
 
+import Dao.ConexaoDao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProdutoModel {
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private double valor;
     
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    public static ProdutoModel obterProdutoPorCodigo (int codigo){
+        String query = "SELECT * FROM PRODUTO WHERE codigo = ?";
+        ResultSet resultSet = null;
+        ProdutoModel prod = null;
+
+        try (Connection conexao = ConexaoDao.conectar();
+            PreparedStatement preparedStatement = conexao.prepareStatement(query)) {
+            preparedStatement.setInt(1, codigo);
+            resultSet = preparedStatement.executeQuery();
+
+            // VERIF SE HÁ RESULTADOS
+            if (resultSet != null && resultSet.next()) {
+                //PREENCHE OBJETO CODIGO COM OS DADOS DO BANCO DE DADOS
+                prod = new ProdutoModel();
+                prod.setCodigo(resultSet.getInt("codigo"));
+                prod.setNome(resultSet.getString("nome"));
+                prod.setDescricao(resultSet.getString("descricao"));
+                prod.setValor(resultSet.getDouble("valor"));
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace(); 
+        } finally {
+            // FECHA O RESULTSET SE PRECISAR
+            try {
+                if (resultSet != null) resultSet.close();
+            } 
+            catch (SQLException e) {
+                e.printStackTrace(); // EXCESSAO SQL SE FECHAR
+            }
+        }
+    return prod;
+    }
 }
